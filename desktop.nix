7c94ce9317fe0bb6efaf54144aea97cbff691073
dkr6 <dylank@posteo.com>
2025-08-25 15:53:29 -0500

    refactor

--- desktop.nix
+++ desktop.nix
@@ -29,24 +29,10 @@
   };
 
   nixpkgs.config.allowUnfree = true;
-
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-
-  networking.hostName = "dylan-laptop"; # Define your hostname.
   networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
-
-  time.timeZone = "America/Chicago";
-
-  i18n.defaultLocale = "en_US.UTF-8";
-  console = {
-    font = "Lat2-Terminus16";
-    useXkbConfig = true; # use xkb.options in tty.
-  };
 
   services.displayManager.gdm.enable = true;
   services.desktopManager.gnome.enable = true;
-
   qt = {
     enable = true;
     platformTheme = "gnome";
@@ -85,10 +71,10 @@
   };
 
   services.libinput.enable = true;
+  programs.steam.enable = true;
 
-  users.users.dylan = {
-    isNormalUser = true;
-    extraGroups = [ "wheel"  "realtime" "audio" ]; # Enable ‘sudo’ for the user.
+  hjem.users.dylan = {
+    enable = true;
     packages = with pkgs; [
       bitwig-studio
       vesktop
@@ -97,13 +83,8 @@
       wineWowPackages.stable
       qbittorrent
       papirus-icon-theme
-      whitesur-kde
+      prismlauncher
     ];
-    password = "7538";
-  };
-
-  hjem.users.dylan = {
-    enable = true;
     directory = "/home/dylan";
     files = {
       ".local/share/wallpaper.jpg".source = ./images/wallpaper.jpg;
@@ -112,33 +93,17 @@
   };
 
   programs.firefox.enable = true;
-  programs.git = {
-    enable = true;
-    package = pkgs.gitFull;
-    config = {
-      user.name = "dkr6";
-      user.email = "dylank@posteo.com";
-      credential = {
-        helper = "manager";
-        credentialStore = "secretservice";
-      };
-    };
-  };
 
   environment.systemPackages = with pkgs; [
     wget
     git-credential-manager
     alacritty
     nautilus
+    emacs
   ] ++ (with pkgs.gnomeExtensions; [
     blur-my-shell
     compiz-windows-effect
     gtk4-desktop-icons-ng-ding
   ]);
-
-
-
-  system.stateVersion = "25.05"; # Did you read the comment?
-
 }
 
