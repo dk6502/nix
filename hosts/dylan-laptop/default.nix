106be0d7ce71666f82c74b564880a45fd3f91cd3
Dylan <krecker@keemail.me>
2025-09-17 12:41:31 -0500

    Update default.nix

--- hosts/dylan-laptop/default.nix
+++ hosts/dylan-laptop/default.nix
@@ -3,7 +3,7 @@ let
 in
 import (sources.nixpkgs + "/nixos/lib/eval-config.nix") {
   modules = [
-    (sources.nixpkgs + "/nixos/modules/profiles/qemu-guest.nix")
+    (sources.nixpkgs + "/nixos/modules/installer/scan/not-detected.nix.nix")
     (sources.disko + "/module.nix")
     (sources.impermanence + "/nixos.nix")
     ../../shared/system.nix
