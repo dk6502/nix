ca3d0b57d1da593c42b15827592877affdf5f1e5
dk6502 <dylank@posteo.com>
2025-10-25 23:43:47 +0000

    smth

--- hosts/oven/oven.nix
+++ hosts/oven/oven.nix
@@ -15,15 +15,11 @@ in
   system.stateVersion = "25.11";
 
   environment.systemPackages = [ pkgs.cifs-utils ];
-  fileSystems."/media" = {
+  fileSystems."/mnt/storage" = {
     device = "//u502897.your-storagebox.de/backup";
     fsType = "cifs";
     options =
-      let
-        automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
-
-      in
-      [ "${automount_opts},credentials=/etc/smb-secrets" ];
+      [ "x-systemd.automount" "noauto" "credentials=/etc/smb-secrets" ];
   };
 
   environment.persistence."/persist" = {
