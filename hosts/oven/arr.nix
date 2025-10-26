48cef0a947522b06422d7a2cdae24cd3e6e82d6c
dk6502 <dylank@posteo.com>
2025-10-26 11:26:40 -0500

    Create arr.nix

---
+++ hosts/oven/arr.nix
@@ -0,0 +1,18 @@
+{
+  services.sonarr = {
+    enable = true;
+    settings = {
+      update.mechanism = "internal";
+      server = {
+        urlbase = "localhost";
+        port = 8989;
+        bindaddress = "*";
+      };
+    };
+  };
+  services.caddy.virtualHosts = {
+    "sonarr.dddk.dev".extraConfig = ''
+      reverse_proxy localhost:8989
+    '';
+  };
+}
