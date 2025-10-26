e51e02701866b80bc5459d73bb1106530ace48d0
dk6502 <dylank@posteo.com>
2025-10-26 15:41:29 -0500

    Update arr.nix

--- hosts/oven/arr.nix
+++ hosts/oven/arr.nix
@@ -15,4 +15,47 @@
       reverse_proxy localhost:8989
     '';
   };
+  environment.persistence."/persist".directories = [
+    "/var/lib/sonarr"
+  ];
+
+  services.radarr = {
+    enable = true;
+    settings = {
+      update.mechanism = "builtIn";
+      server = {
+        urlbase = "localhost";
+        port = 7878;
+        bindaddress = "*";
+      };
+    };
+  };
+  services.caddy.virtualHosts = {
+    "radarr.dddk.dev".extraConfig = ''
+      reverse_proxy localhost:7878
+    '';
+  };
+  environment.persistence."/persist".directories = [
+    "/var/lib/radarr"
+  ];
+
+  services.prowlarr = {
+    enable = true;
+    settings = {
+      update.mechanism = "builtIn";
+      server = {
+        urlbase = "localhost";
+        port = 9696;
+        bindaddress = "*";
+      };
+    };
+  };
+  services.caddy.virtualHosts = {
+    "prowlarr.dddk.dev".extraConfig = ''
+      reverse_proxy localhost:9696
+    '';
+  };
+  environment.persistence."/persist".directories = [
+    "/var/lib/prowlarr"
+  ];
 }
