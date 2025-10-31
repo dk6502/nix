12562275c27b6f55bef3f8f6a71c6edd46e6c4a8
dk6502 <dylank@posteo.com>
2025-10-31 13:43:22 -0500

    komga

---
+++ hosts/oven/komga.nix
@@ -0,0 +1,11 @@
+{
+  services.komga {
+    enable = true;
+    settings.server.port = 6161;
+  }
+  services.caddy.virtualHosts = {
+    "komga.dddk.dev".extraConfig = ''
+      reverse_proxy localhost:6161
+    '';
+  };
+}
