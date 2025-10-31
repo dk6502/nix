bedbee3884418b918227fadd248a818f689cfb39
dk6502 <dylank@posteo.com>
2025-10-31 13:52:45 -0500

    Update komga.nix

--- hosts/oven/komga.nix
+++ hosts/oven/komga.nix
@@ -2,7 +2,7 @@
   services.komga = {
     enable = true;
     settings.server.port = 6161;
-  }
+  };
   services.caddy.virtualHosts = {
     "komga.dddk.dev".extraConfig = ''
       reverse_proxy localhost:6161
